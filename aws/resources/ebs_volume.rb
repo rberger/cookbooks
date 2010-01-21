38c7c35255a9ab1716bc51cdca9d08552dd28c96
Robert J. Berger <rberger@terra.runa.com>
2010-01-20 22:13:43 -0800

    Make ec2 work with regions

--- aws/resources/ebs_volume.rb
+++ aws/resources/ebs_volume.rb
@@ -9,3 +9,4 @@ attribute :device,                :kind_of => String
 attribute :volume_id,             :kind_of => String
 attribute :timeout,               :default => 3*60 # 3 mins, nil or 0 for no timeout
 attribute :snapshots_to_keep,     :default => 2
+attribute :region,                :default => "us-east-1"
