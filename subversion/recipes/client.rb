ca74e995ebee1300662659bf3b208aea2e9b548b
Robert J. Berger <rberger@ibd.com>
2010-09-08 15:58:01 -0700

    Fixed as suggested COOK-358

--- subversion/recipes/client.rb
+++ subversion/recipes/client.rb
@@ -25,6 +25,8 @@ extra_packages = case node[:platform]
   when "ubuntu"
     if node[:platform_version].to_f < 8.04
       %w{subversion-tools libsvn-core-perl}
+    else
+      %w{subversion-tools libsvn-perl}
     end
   when "centos","redhat","fedora"
     %w{subversion-devel subversion-perl}
@@ -32,10 +34,8 @@ extra_packages = case node[:platform]
     %w{subversion-tools libsvn-perl}
   end
 
-if extra_packages
-  extra_packages.each do |pkg|
-    package pkg do
-      action :install
-    end
+extra_packages.each do |pkg|
+  package pkg do
+    action :install
   end
 end
