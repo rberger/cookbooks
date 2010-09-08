2cb5390e08c7c0c2b1cd948dbe54d266efd8d359
Robert J. Berger <rberger@ibd.com>
2010-09-08 15:24:52 -0700

    Added a test for extra_packages before extra_packages.each

--- subversion/recipes/client.rb
+++ subversion/recipes/client.rb
@@ -32,8 +32,10 @@ extra_packages = case node[:platform]
     %w{subversion-tools libsvn-perl}
   end
 
-extra_packages.each do |pkg|
-  package pkg do
-    action :install
+if extra_packages
+  extra_packages.each do |pkg|
+    package pkg do
+      action :install
+    end
   end
 end
